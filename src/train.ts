085f5f7b3b21f145275cc8a77ab26f3f26834be8
Max060794 <mironhanov@gmail.com>
2024-02-26 22:15:17 +0900

    feat: Environmental variablelar, database va Express & MITASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,9 @@
-console.log("mofo");
+// Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+// MASALAN: gerReverse("hello") return qiladi "olleh"
+
+
+function getReverse(inputString: string): string {
+    return inputString.split('').reverse().join('');
+}
+const reversedString: string = getReverse("hello");
+console.log(reversedString); 
